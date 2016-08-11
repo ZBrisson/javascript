62355039b5b1e1f0aab6b1b2b203bc5346dc3141
Zach Brisson <github@zachbrisson.com>
2016-08-11 18:45:01 -0400

    Create addressbook.js
    Vanilla code for Codecademy's code your own adventure part 2. Link can be found as follows:
    https://www.codecademy.com/courses/building-an-address-book/0/1

---
+++ addressbook.js
@@ -0,0 +1,48 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function add(firstName, lastName, email, phoneNumber) {
+    contacts[contacts.length] = {
+        firstName: firstName,
+        lastName: lastName,
+        phoneNumber: phoneNumber,
+        email: email
+    }
+}
+
+function printPerson(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+
+function list() {
+	var contactsLength = contacts.length;
+	for (var i = 0; i < contactsLength; i++) {
+		printPerson(contacts[i]);
+	}
+}
+
+/*Create a search function
+then call it passing "Jones"*/
+function search (lastName) {
+	var contactsLength = contacts.length;
+	for (var i = 0; i < contactsLength; i++) {
+	    if (lastName === contacts[i].lastName) {
+	        printPerson(contacts[i])
+	    }
+	}
+}
+add("Zach", "Brisson", "(555) 555-5555", "zach.brisson@example.com");
+list(contacts)
