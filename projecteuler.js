c665d1b7eee1418e0744bca03971691b59dbfa80
Zach Brisson <github@zachbrisson.com>
2016-08-17 14:38:12 -0400

    Update projecteuler.js
    Used techniques learned from evenFibonacci and applied those to previous multiplesOf3And5 function to trim code down.

--- projecteuler.js
+++ projecteuler.js
@@ -1,16 +1,30 @@
-function MultiplesOf3And5 (range) {
+function multiplesOf3And5 (range) {
 	var total = 0;
 	for (var i = 0; i < range; i++) {
-		if (i % 3 === 0) {
+		if (i % 3 === 0 || i % 5 === 0 ) {
 			total += i;
-		}
-		else if (i % 5 === 0) {
-			total += i;
-		}
-		else {
-			
 		}
 		
 	}
 	console.log(total);
+}
+
+function evenFibonacci (range) {
+	var previousX = 0;
+	var currentX = 1;
+	var nextX;
+	var totalSum = 0;
+	
+	for (i = 1; currentX <= range; i++) {
+
+		
+		nextX = currentX + previousX;
+		previousX = currentX;
+		currentX = nextX;
+		if (currentX % 2 === 0) {
+			totalSum += currentX;
+			console.log(totalSum);
+		}
+	}
+	
 }
