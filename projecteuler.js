b88984f3b08646838dbff0be61d51eb4ff66b130
Zach Brisson <github@zachbrisson.com>
2016-08-18 14:29:56 -0400

    Update projecteuler.js

--- projecteuler.js
+++ projecteuler.js
@@ -28,3 +28,24 @@ function evenFibonacci (range) {
 	}
 	
 }
+function primeFactors (range) {
+	var primeArray = [];
+
+	// divide our number by 2 until it can't be divided anymore
+	while (range % 2 === 0) {
+		range = range / 2;
+	}
+	// remaining prime must be odd. 
+	//start with odd number and increment by 2.
+	for (i = 3; i <= Math.sqrt(range); i = i + 2) {
+		// show primes that can be divided after square root eliminates divisible numbers.
+		while (range % i === 0) {
+			console.log(i);
+			range = range / i;
+		}
+	}
+	if (range > 2) {
+		console.log(range);
+	}
+}
+
